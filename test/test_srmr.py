24ff99864a6ef9168e0f5a5b6c14d3a901c773b2
Joao Felipe Santos <joao.eel@gmail.com>
2014-12-30 12:46:04 -0500

    Fixed tests

--- test/test_srmr.py
+++ test/test_srmr.py
@@ -6,23 +6,19 @@ import matplotlib.pyplot as plt
 def test_srmr():
     fs = 16000
     s = loadmat("test/test.mat")["s"][:,0]
+
+    correct_ratios = loadmat("test/correct_ratios.mat")['correct_ratios'][0]
     ratio, avg_energy = srmr(s, fs)
-    #plt.imshow(np.flipud(avg_energy))
-    #plt.show()
-    print("Ratio (fast): %2.4f" % ratio)
-    assert np.allclose(ratio, 6.062267651334784, rtol=1e-6, atol=1e-12)
+    assert np.allclose(ratio, correct_ratios[1], rtol=1e-6, atol=1e-12)
 
     ratio_norm_fast, avg_energy_norm_fast = srmr(s, fs, fast=True, norm=True, max_cf=30)
-    print("Ratio (norm, fast): %2.4f" % ratio_norm_fast)
-    
+    assert np.allclose(ratio_norm_fast, correct_ratios[2], rtol=1e-6, atol=1e-12)
+
     ratio_slow, avg_energy_slow = srmr(s, fs, fast=False)
-    print("Ratio (slow): %2.4f" % ratio_slow)
+    assert np.allclose(ratio_slow, correct_ratios[0], rtol=1e-6, atol=1e-12)
 
     ratio_norm, avg_energy_norm = srmr(s, fs, fast=False, norm=True, max_cf=30)
-    print("Ratio (norm): %2.4f" % ratio_norm)
-    
-
-
+    assert np.allclose(ratio_norm, correct_ratios[3], rtol=1e-6, atol=1e-12)
 
 if __name__ == '__main__':
     test_srmr()

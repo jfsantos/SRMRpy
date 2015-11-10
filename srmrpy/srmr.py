Fixed VAD and factored normalization out of srmr function

srmrpy/srmr.py
@@ -31,6 +31,13 @@ def calc_cutoffs(cfs, fs, q):
     R = cfs + (B0 * fs / (2*np.pi))
     return L, R
 
+def normalize_energy(energy, drange=30.0):
+    peak_energy = np.max(np.mean(energy, axis=0))
+    min_energy = peak_energy*10.0**(-drange/10.0)
+    energy[energy < min_energy] = min_energy
+    energy[energy > peak_energy] = peak_energy
+    return energy
+
 def srmr(x, fs, n_cochlear_filters=23, low_freq=125, min_cf=4, max_cf=128, fast=True, norm=False):
     wLengthS = .256
     wIncS = .064
@@ -62,10 +69,7 @@ def srmr(x, fs, n_cochlear_filters=23, low_freq=125, min_cf=4, max_cf=128, fast=
             energy[i,j,:] = np.sum((w*mod_out_frame[:n_frames])**2, axis=1)
 
     if norm:
-        peak_energy = np.max(np.mean(energy, axis=0))
-        min_energy = peak_energy*0.001
-        energy[energy < min_energy] = min_energy
-        energy[energy > peak_energy] = peak_energy
+        energy = normalize_energy(energy)
 
     erbs = np.flipud(calc_erbs(low_freq, fs, n_cochlear_filters))
 

srmrpy/vad.py
@@ -15,13 +15,13 @@ def simple_energy_vad(x, fs, framelen=0.02, theta_main=30, theta_min=-55):
     frame_energy = 10*np.log10(1/(framelen-1) * (frames_zero_mean**2).sum(axis=1) + 1e-6)
     max_energy = max(frame_energy)
     speech_presence = (frame_energy > max_energy - theta_main) & (frame_energy > theta_min)
-    x_vad = np.zeros_like(x)
+    x_vad = np.zeros_like(x, dtype=bool)
     for idx, frame in enumerate(frames):
         if speech_presence[idx]:
-            x_vad[idx*framelen:(idx+1)*framelen] = s[idx*framelen:(idx+1)*framelen]
+            x_vad[idx*framelen:(idx+1)*framelen] = True
         else:
-            x_vad[idx*framelen:(idx+1)*framelen] = 0
-    return x_vad, speech_presence
+            x_vad[idx*framelen:(idx+1)*framelen] = False
+    return x[x_vad], x_vad
 
 if __name__ == '__main__':
     import sys
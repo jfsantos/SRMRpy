426082196d96776e608aff125ecfbe062a7b3579
Changsheng Quan <quancs@qq.com>
2023-06-09 16:56:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires = [
         'numpy',
         'scipy',
-        'Gammatone @ https://github.com/detly/gammatone/archive/master.zip#egg=Gammatone',
+        'gammatone @ git+https://github.com/detly/gammatone',
     ],
 
     tests_require = [
